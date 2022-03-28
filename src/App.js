1c508bb9cfaf836f678023e13bc701496c63929f
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-03-28 01:35:57 +0100

    Search_Filter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {User} from "./User";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input text="text" placeholder='Search' className='search'></input>
+      <ul className='list'>
+        {User.map((User) =>(
+          <li className='listItem'>{User.first_name}</li>
+        ))};
+        
+        
+
+      </ul>
     </div>
   );
 }
---
+++ src/User.js
@@ -0,0 +1,97 @@
+import { getDefaultNormalizer } from "@testing-library/react";
+
+export const User = [
+    {
+        "id": 1,
+        "first_name":"Ifeoluwa",
+        "last_name": "Oladeni",
+        "email": "Oladeniifeoluwa@gmail.com",
+        "gender:": "Male",
+    },
+    {
+        "id": 2,
+        "first_name":"idapo",
+        "last_name": "Oladeni",
+        "email": "Oladeniidapogmail.com",
+        "gender:": "Male",
+    },
+    {
+        "id": 3,
+        "first_name":"ore",
+        "last_name": "Oladeni",
+        "email": "Oladeniore@gmail.com",
+        "gender:": "Female",
+    },
+    {
+        "id": 4,
+        "first_name":"esther",
+        "last_name": "Oladeni",
+        "email": "Oladeniesther@gmail.com",
+        "gender:": "Female",
+    },
+    {
+        "id": 5,
+        "first_name":"sola",
+        "last_name": "Oladeni",
+        "email": "Oladenisola@gmail.com",
+        "gender:": "Male",
+    },
+    {
+        "id": 6,
+        "first_name":"segun",
+        "last_name": "Olabisi",
+        "email": "segunolabisi@gmail.com",
+        "gender:": "Male",
+    },
+    {
+        "id": 7,
+        "first_name":"timi",
+        "last_name": "seg",
+        "email": "timiseg@gmail.com",
+        "gender:": "Male",
+    },
+    {
+        "id": 8,
+        "first_name":"favour",
+        "last_name": "abioye",
+        "email": "favourabioye@gmail.com",
+        "gender:": "female",
+    },
+    {
+        "id": 9,
+        "first_name":"eunice",
+        "last_name": "sam",
+        "email": "eunicesam@gmail.com",
+        "gender:": "Male",
+    },
+    {
+        "id": 10,
+        "first_name":"rhoda",
+        "last_name": "babatunde",
+        "email": "rhodababtunde@gmail.com",
+        "gender:": "famle",
+    },
+    {
+        "id": 11,
+        "first_name":"sade",
+        "last_name": "adeoye",
+        "email": "sadeadeoye@gmail.com",
+        "gender:": "female",
+    },
+    {
+        "id": 12,
+        "first_name":"emma",
+        "last_name": "greg",
+        "email": "emmagreg@gmail.com",
+        "gender:": "Male",
+    },
+    {
+        "id": 13,
+        "first_name":"femi",
+        "last_name": "olu",
+        "email": "femiolu@gmail.com",
+        "gender:": "Male",
+    },
+    
+]
+export default User;
